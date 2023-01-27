f42c5a1fb260c56efa4dbe3d50966da1a2d53793
Neon-jeff <omaksteejay@gmail.com>
2023-01-27 21:48:05 +0100

    Fixed Api Home

--- menu/urls.py
+++ menu/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api',include('order.urls'))
+    path('api',include('order.urls')),
+    path('',TemplateView.as_view(template_name='home.html'))
+
+
 ]
